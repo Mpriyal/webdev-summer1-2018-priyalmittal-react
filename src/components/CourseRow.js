17d02302a79b7ce41aff73669f2c4a7fcf5ed146
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-21 01:08:57 -0400

    Modified Course Row, List and Service

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import CourseService from "../services/CourseService";
 class CourseRow extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.courseService = CourseService.instance;
+    }
+
     render() {
         return (
             <tr>
-                <td>Course Row</td>
+                <td>{this.props.course.title}</td>
+                <td>{this.props.course.modified}</td>
+                <td>
+                    <button className="btn btn-danger"
+                        onClick={() =>
+                        {this.props.delete(this.props.course.id)}}>
+                    Delete
+                    </button>
+                </td>
             </tr>
         )
     }
--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,25 +1,110 @@
 import React from 'react';
 import CourseRow from "../components/CourseRow";
+import CourseService from "../services/CourseService"
+let self
+
 class CourseList extends React.Component {
+
     constructor() {
         super();
+        this.courseService = CourseService.instance;
+        this.state = {courses: []};
+        self = this;
+        this.courseTitleChanged = this.courseTitleChanged.bind(this);
+        this.createCourse = this.createCourse.bind(this);
+        this.deleteCourse = this.deleteCourse.bind(this);
+        }
+
+    componentDidMount() {
+        this.findAllCourses();
     }
+
+    findAllCourses() {
+        this.courseService
+            .findAllCourses()
+            .then((courses) => {
+                console.log(courses);
+                this.setState({courses: courses});
+            })
+    }
+
+    courseTitleChanged(event) {
+        this.setState({
+            course:{title: event.target.value}
+        });
+    }
+
+    createCourse() {
+        this.courseService
+            .createCourse(this.state.course)
+            .then(() => { this.findAllCourses();}
+            );
+    }
+
+    deleteCourse(courseId) {
+        console.log('delete id: '+courseId);
+        this.courseService.deleteCourse(courseId).then(
+            () => {
+                this.findAllCourses();
+            }
+        );
+    }
+
+    renderCourseRows() {
+        let courses = null;
+        if(this.state) {
+            courses = this.state.courses.map(
+                function (course) {
+                    return <CourseRow key={course.id}
+                                      course={course}
+                                      delete={self.deleteCourse}/>
+                }
+            );
+        }
+        return (
+            courses
+        )
+    }
+
     render() {
         return (
             <div>
-            <h2>Course List</h2>
+            {/*<h2>Course List</h2>*/}
             <table className="table">
             <thead>
+            {/*<tr>*/}
+                {/*<th>*/}
+                    {/*<input className="form-control"*/}
+                           {/*id="titleFld"*/}
+                           {/*onChange={this.courseTitleChanged}*/}
+                           {/*placeholder="New Course Title"/>*/}
+                {/*</th>*/}
+                {/*<th>*/}
+                    {/*<button className="btn btn-primary"*/}
+                            {/*onClick={this.createCourse}>*/}
+                        {/*Add*/}
+                    {/*</button>*/}
+                {/*</th>*/}
+            {/*</tr>*/}
+            <nav className="navbar navbar-light navbar-expand">
+                <div className="container">
+                    <a className="navbar-brand color-white" href="#">Course Manager {this.state.courseId}</a>
+                    <input className="form-control" id="titleFld"
+                           onChange={this.courseTitleChanged}
+                           placeholder="New Course Title "/>
+                    <button className="btn btn-primary"
+                    onClick={this.createCourse}>
+                    Add
+                    </button>
+                </div>
+            </nav>
             <tr>
                 <th>Title</th>
+                <th>Last Modified</th>
             </tr>
             </thead>
         <tbody>
-        <CourseRow/>
-        <CourseRow/>
-        <CourseRow/>
-        <CourseRow/>
-        <CourseRow/>
+        {this.renderCourseRows()}
         </tbody>
         </table>
             </div>
