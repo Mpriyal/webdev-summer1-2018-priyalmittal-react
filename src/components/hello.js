774572a71f79ceebaf927da10fd409f0eea5b814
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-22 19:55:17 -0400

    Added Stateless and hello files to avoid error and modified ModuleListItem

---
+++ src/components/hello.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>Hello World!!</h1>
+        )
+    }
+}
+
+export default Hello;
