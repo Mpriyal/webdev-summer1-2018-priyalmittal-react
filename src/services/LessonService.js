e91ec35225a178d62901d1921d9bcdd6a2494d65
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-23 18:17:58 -0400

    Modified Lesson and Module services

---
+++ src/services/LessonService.js
@@ -0,0 +1,58 @@
+const LESSON_API_URL = 'http://localhost:8080/api/course/CID/module/MID/lesson';
+const LESSON_API = 'http://localhost:8080/api/lesson';
+let _singleton = Symbol();
+
+export default class LessonService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Singleton!!!');
+    }
+
+    static get instance() {
+        if(!this[_singleton])
+            this[_singleton] = new LessonService(_singleton);
+        return this[_singleton]
+    }
+
+    createLesson(courseId, moduleId, lesson) {
+        return fetch(LESSON_API_URL.replace('CID', courseId).replace('MID', moduleId),
+            {
+                body: JSON.stringify(lesson),
+                headers: { 'Content-Type': 'application/json' },
+                method: 'POST'
+            }).then(function (response)
+        { return response.json();
+        }
+        )
+    }
+
+    findAllLessons() {
+        return fetch(
+            LESSON_API)
+            .then(function (response) {
+                return response.json();
+            }
+            )
+    }
+
+    findAllLessonsForModule(courseId, moduleId) {
+        console.log(courseId);
+        console.log(moduleId);
+        return fetch(
+            LESSON_API_URL.replace('CID',courseId).replace('MID', moduleId))
+            .then(function (response) {
+                return response.json();
+            })
+    }
+
+    deleteLesson(lessonId) {
+        return fetch(LESSON_API+ '/' +lessonId,
+            {
+                method: 'DELETE'
+            }).then(function (response) {
+            return response;
+        })
+    }
+
+
+}
