6276dd3d793c03b0b2e9bd40ab9a832d533b5141
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-23 00:29:45 -0400

    Modified Lesson Tab Item and added link

--- src/components/LessonTabItem.js
+++ src/components/LessonTabItem.js
@@ -1,19 +1,24 @@
 import React from 'react';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-export default class LessonListItem
+export default class LessonTabItem
     extends React.Component {
     constructor(props) {
         super (props);
     }
     render() {
         return (
-            <div>
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">
-                        {this.props.title}</a>
-                </li>
-            </div>
+            <li className="nav-item">
+                <Link to={`/course/${this.props.courseId}/module/${this.props.moduleId}`}>
+                    {this.props.lesson.title}
+                </Link>
+                <span className="float-right">
+                    <button onClick={() =>
+                    {this.props.delete(this.props.lesson.id)}}>
+                <i className="fa fa-trash"></i>
+                    </button>
+                </span>
+            </li>
         );
     }
 }
