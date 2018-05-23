4adffdef2c4950ad9fedde57ee30829735b7249b
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-23 01:21:41 -0400

    Fixed bugs from CourseEdit

--- src/containers/CourseEdit.js
+++ src/containers/CourseEdit.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ModuleList2 from './ModuleList2'
-import LessonTabs from './LessonTabs'
-import ModuleEditor from "./ModuleEditor";
 
 export default class CourseEdit
     extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {courseId: '',
-                moduleId: ''};
+        this.state = {
+            courseId: '',
+            moduleId: ''
+        };
         this.selectCourse = this.selectCourse.bind(this);
         this.selectModule = this.selectModule.bind(this);
     }
@@ -17,7 +17,6 @@ export default class CourseEdit
     componentDidMount() {
         this.selectCourse(this.props.match.params.courseId);
         this.selectModule(this.props.match.params.moduleId);
-
     }
 
     selectCourse(courseId) {
@@ -29,37 +28,17 @@ export default class CourseEdit
     }
 
     render() {
-        if(this.state.moduleId!=undefined){
-            return (
-                <div>
-                    <h2>Editing Course: {this.state.courseId}</h2>
-                    <div className="row">
-                        <div className="col-4">
-                            <ModuleList2 courseId={this.state.courseId}/>
-                        </div>
-                        <div className="col-8">
-                            <LessonTabs
-                                courseId = {this.state.courseId}
-                                moduleId = {this.state.moduleId}
-                            />
-                        </div>
+        return (
+            <div>
+                <h2>Editing Course: {this.state.courseId}</h2>
+                <div className="row">
+                    <ModuleList2 courseId={this.state.courseId}/>
+                    <div className="col-8">
                     </div>
                 </div>
-            );
-        }
-        else {
-            return (
-                <div>
-                    <h2>Editing Course: {this.state.courseId}</h2>
-                    <div className="row">
-                        <div className="col-4">
-                            <ModuleList2 courseId={this.state.courseId}/>
-                        </div>
-                        <div className="col-8">
-                        </div>
-                    </div>
-                </div>
-            );
-        }
+            </div>
+        );
     }
+
+    // }
 }
