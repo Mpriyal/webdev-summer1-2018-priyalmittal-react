e107191f5e1fe80a6b8fc43f8c84d59d39aab160
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-23 22:00:51 -0400

    Modified module list to remove unwanted comments

--- src/containers/ModuleList.js
+++ src/containers/ModuleList.js
@@ -60,7 +60,6 @@ class ModuleList extends React.Component {
             this.moduleService
                 .deleteModule(courseId, moduleId).then(
                 () => {
-                    // this.findAllModulesForCourse(courseId);
                     window.location.href = '/course/' + courseId;
                 }
             );
